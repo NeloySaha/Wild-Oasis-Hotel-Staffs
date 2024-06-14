991a4e9d32ac1819c8d08c3677173ebeaedf219e
Neloy Saha <neloy.saha456@gmail.com>
2024-06-15 04:46:34 +0600

    minor change

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("neloy@example.com");
-  const [password, setPassword] = useState("neloy123");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const { loginMutation, isLoading } = useLogin();
 
   function handleSubmit(e) {
