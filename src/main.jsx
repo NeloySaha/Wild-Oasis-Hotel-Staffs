47dd94ac1baa0c847f2800fc9244dd6e2ab9a69a
Neloy Saha <neloy.saha456@gmail.com>
2024-06-29 21:01:01 +0600

    added vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { ErrorBoundary } from "react-error-boundary";
 import ErrorFallback from "./ui/ErrorFallback.jsx";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -12,5 +13,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     >
       <App />
     </ErrorBoundary>
+    <Analytics />
   </React.StrictMode>
 );
