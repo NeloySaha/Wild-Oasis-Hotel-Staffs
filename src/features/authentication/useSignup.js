7456ee150a03a1572e36ae7e913363dd526be3a4
Neloy Saha <neloy.saha456@gmail.com>
2024-09-20 11:07:31 +0600

    removed email verifying

--- src/features/authentication/useSignup.js
+++ src/features/authentication/useSignup.js
@@ -1,14 +1,15 @@
 import { useMutation } from "@tanstack/react-query";
 import { signUp } from "../../services/apiAuth";
 import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 export const useSignup = () => {
+  const navigate = useNavigate();
   const { mutate: signUpMutation, isPending: isLoading } = useMutation({
     mutationFn: signUp,
     onSuccess: () => {
-      toast.success(
-        "New Account created successfully! Please, check your email to verify your account"
-      );
+      toast.success("Your account is created successfully!");
+      navigate("/dashboard");
     },
   });
 
